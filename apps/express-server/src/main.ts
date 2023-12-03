4530dc52338a4b118be2df3ace30c83924c947ed
tsuchidashinya <tsuchida.shinya413@mail.kyutech.jp>
2023-12-03 12:37:41 +0900

    fix: express_serverにcorsの設定を入れる

--- apps/express-server/src/main.ts
+++ apps/express-server/src/main.ts
@@ -2,6 +2,7 @@ import env from 'dotenv'
 import express from 'express'
 import appRoute from './router/index'
 import { createDeviceData } from './common/createData'
+import cors from 'cors'
 
 env.config()
 const port = process.env.PORT || 8000
@@ -9,6 +10,12 @@ console.log(port)
 
 const app = express()
 app.use(express.json())
+app.use(
+  cors({
+    origin: /^http:\/\/localhost.*$/,
+    credentials: true,
+  }),
+)
 app.use('/', appRoute)
 
 app.listen(port, () => {
