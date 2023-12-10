e440aa09a64d4f6d37c8a1422bec0acc5934acf8
tsuchidashinya <tsuchida.shinya413@mail.kyutech.jp>
2023-12-11 00:14:38 +0900

    fix: tableのstory

--- packages/ui-library/src/components/MUI/Table.stories.tsx
+++ packages/ui-library/src/components/MUI/Table.stories.tsx
@@ -1,8 +1,9 @@
 import { Table, TableOwnProps, TableBody, TableCell, TableHead, TableRow } from '@mui/material'
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid'
 export default {
   title: 'MUI/Table',
   component: Table,
-  args: { stickyHeader: false } as TableOwnProps,
+  args: { stickyHeader: false, padding: 'none', size: 'medium' } as TableOwnProps,
 }
 
 function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
@@ -22,12 +23,14 @@ export const All = ({ ...args }: TableOwnProps) => {
     <>
       <div style={componentStyle}>
         MUIのテーブル
-        <Table>
+        <Table {...args}>
           <TableHead>
             <TableRow>
               <TableCell>Dessert (100g serving)</TableCell>
               <TableCell align='right'>Calories</TableCell>
-              <TableCell align='right'>Fat&nbsp;(g)</TableCell>
+              <TableCell align='right' scope={}>
+                Fat&nbsp;(g)
+              </TableCell>
               <TableCell align='right'>Carbs&nbsp;(g)</TableCell>
               <TableCell align='right'>Protein&nbsp;(g)</TableCell>
             </TableRow>
@@ -72,6 +75,59 @@ export const All = ({ ...args }: TableOwnProps) => {
             ))}
           </tbody>
         </table>
+      </div>
+    </>
+  )
+}
+
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'firstName', headerName: 'First name', width: 130 },
+  { field: 'lastName', headerName: 'Last name', width: 130 },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 90,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: (params: GridValueGetterParams) =>
+      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+  },
+]
+
+const rows1 = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+]
+
+export const XDataGrid = () => {
+  return (
+    <>
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+          rows={rows1}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 5 },
+            },
+          }}
+          pageSizeOptions={[5, 10]}
+          checkboxSelection
+        />
       </div>
     </>
   )
