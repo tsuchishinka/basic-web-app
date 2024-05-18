8ac43a9720120c0c845903539a6dac3a127b2d99
tsuchidashinya <tsuchida.shinya413@mail.kyutech.jp>
2024-05-18 15:59:56 +0900

    fix: APIサービスを別ワークスペースに切り出し

---
+++ packages/demoAPI/src/service/device/types.ts
@@ -0,0 +1,25 @@
+type ListResponse<T> = {
+  offset: number;
+  total: number;
+  count: number;
+  list: Array<T>;
+};
+
+type RequestFetchDevice = {
+  name?: string;
+  offset?: number;
+  limit?: number;
+};
+
+type ResponseFetchDevice = {
+  offset: number;
+  total: number;
+  list: {
+    deviceId: string;
+    name: string;
+    model: string;
+  }[];
+  count: number;
+};
+
+export type { ListResponse, RequestFetchDevice, ResponseFetchDevice };
