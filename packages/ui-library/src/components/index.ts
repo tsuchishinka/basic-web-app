9af3ff79dadbcb98324f871ecba0975b7d17c16a
tsuchidashinya <tsuchishinka@gmail.com>
2024-10-20 02:30:10 +0900

    fix: ui-libraryの微調整

--- packages/ui-library/src/components/index.ts
+++ packages/ui-library/src/components/index.ts
@@ -2,5 +2,7 @@ export * from './Button'
 export * from './Input/TextInput'
 export * from './Input/InputWrapper'
 export * from './Table/SimpleTable/SimpleTable'
+export * from './Other/Loading'
+export * from './Time/DateTimeTextInput'
 
 import '../utils/common.scss'
